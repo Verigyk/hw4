2d0c2093dafca7fae96218912d469d09cf3b8542
Verigyk <47785872+Verigyk@users.noreply.github.com>
2025-11-14 11:24:33 +0100

    HIII

--- sample/hw4.cu
+++ sample/hw4.cu
@@ -33,6 +33,11 @@
 typedef unsigned int WORD;
 typedef unsigned char BYTE;
 
+typedef union _sha256_ctx{
+	WORD h[8];
+	BYTE b[32];
+}SHA256;
+
 __managed__ WORD k[64] = {
 	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
 	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
